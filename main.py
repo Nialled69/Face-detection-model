4443e09ffce1998988876ad6c64f57d670bbbbf5
Nialled69 <dj.nialled.007.1d@gmail.com>
2025-09-07 21:04:50 +0530

    changes pushed

---
+++ main.py
@@ -0,0 +1,3 @@
+from ultralytics import yolo
+import numpy as np
+import pandas as pd
--- test.py
+++ test.py
@@ -2,14 +2,13 @@ from ultralytics import YOLO
 import cv2
 import numpy as np
 
-model = YOLO("yolov11n-face.pt")
+model = YOLO("models/yolov11n-face.pt")
 
 cap = cv2.VideoCapture(0)
 
 if not cap.isOpened():
     print("[ERROR] Could not open webcam.")
     exit()
-
 print("[INFO] Starting face detection...")
 
 while True:
@@ -26,7 +25,7 @@ while True:
             cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
             cv2.putText(frame, "Face", (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2)
 
-    cv2.imshow("YOLOv8 Face Detection", frame)
+    cv2.imshow("YOLOv11 Face Detection", frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
